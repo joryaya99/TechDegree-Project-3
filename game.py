139c8c00a24a85efc366e0eba8016c942bc06d48
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-03-09 11:27:08 -0800

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,18 @@
 from phrase import Phrase
 import random
+
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.phrases = []
+        self.guesses = [" "]
+        self.phrases = self.create_phrases()
+        self.active_phrase = self.get_random_phrase()
+        
+    def get_random_phrase(self):
+        
+    def welcome(self):
+        
+    def get_guess(self):
+        
+    def game_over(self):
