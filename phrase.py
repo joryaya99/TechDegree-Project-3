847f3fcdaa19631e575d44565c4340329dc44df0
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-03-09 10:41:44 -0800

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -3,6 +3,8 @@ class Phrase:
         self.phrase = phrase.lower()
     
     def display(self, guesses):
+        for letter in self.phrase:
+            if letter in guesses:
     
     def check_letter(self, guess):
     
