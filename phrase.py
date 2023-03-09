8f9c7a25fb11dfe3b498247d8927edd70f70dff3
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-03-09 10:34:45 -0800

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -1,9 +1,9 @@
 class Phrase:
-  
-  def __init__(self, phrase):
+    def __init__(self, phrase):
+        self.phrase = phrase.lower()
     
-  def display(self, guesses):
+    def display(self, guesses):
     
-  def check_letter(self, guess):
+    def check_letter(self, guess):
     
-  def check_complete(self, guesses):
+    def check_complete(self, guesses):
