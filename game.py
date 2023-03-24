bba0ac137229f392b1fe36fe84dc4380f71f8708
joryaya99 <110695145+joryaya99@users.noreply.github.com>
2023-03-24 11:59:44 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,74 @@
 from phrase import Phrase
 import random
+import os
+
 
 class Game:
 
     def __init__(self):
-        self.phrases = ["Hello world !",
-			"How are you ?",
-			"I'm John",
-			"It's nice to meet you !"
-			"What's your favorite color ?"
-			]
-        self.active_phrase = Phrase(self.phrases[random.randint(0, len(self.phrases) - 1)])
-        self.guesses = [' ']
-        self.game_continues = True
-        self.missed = 0
+        self.missed = 5
+        self.phrases = self.create_phrase()
+        self.active_phrase = self.get_random_phrase()
+        self.guesses = [' ',"'",'!',',']
 
-    def start(self):
-        print("Welcome to the Phrase Hunter !!!")
-        print("Guess the phrase before you run out of guesses.")
-        print("You have a total of 5 guesses to guess the Phrase.")
-        print("There is no certain format to follow, you do not have to include punctuation in your guesses.")
-        print("However Capitalization is important")
 
-        self.active_phrase.display(self.guesses)
+    def create_phrase(self):
+        movie_quotes = [
+            "May the Force be with you",
+            "There's no place like home",
+            "I'm the king of the world",
+            "I'll be back",
+            "There's no crying in baseball",
+            "If you build it, he will come",
+            "The stuff that dreams are made of",
+            "Keep your friends close, but your enemies closer",
+            "I am your father",
+            "Just keep swimming"
+        ]
+        phrases = []
+        for movie_quote in movie_quotes:
+            phrases.append(Phrase(movie_quote))
+        return phrases
 
-        while self.missed != 5 and self.game_continues:
-            self.attempt = input("\nEnter a letter: ")
-            if self.active_phrase.check_letter(self.attempt):
-                self.guesses.append(self.attempt)
-            else:
-                self.missed += 1
-                print(f"Incorrect! You have {5 - self.missed} out of 5 guesses left.")
+
+    def start(self, user_guess):
+        self.welcome()
+        while self.missed > 0 and not self.active_phrase.check_complete(self.guesses):
+            print("~" * 22)
+            print(f' # of tries left : {self.missed}')
+            print("~" * 22)
+            
             self.active_phrase.display(self.guesses)
-            if Phrase.check_complete(self, self.active_phrase.chosenPhrase, self.guesses):
-                print("\nYay !!! Congratulations you've won !!!")
-                self.game_continues = False
-        if self.missed == 5:
-            print("\nOh no ! Seems like you've lost !")
+            user_guess = self.get_guess()
+            self.guesses.append(user_guess)
+            
+            if not user_guess.isalpha():
+                print("Letters can only be used, please try again.")
+            elif len(user_guess) > 1:
+                print("Please enter one letter at a time.")
+            elif not self.active_phrase.check_letter(user_guess):
+                self.missed -= 1
+            os.system("clear")
+        self.game_over()
+
+    def get_random_phrase(self):
+        return random.choice(self.phrases)
+
+
+    def welcome(self):
+        print("")
+        print("=" * 25)
+        print(" Guess that Phrase ! ")
+        print("=" * 25)
+        print("")
+
+    def get_guess(self):
+        guess = input("Enter a letter: ")
+        return guess.lower()
+
+    def game_over(self):
+        if self.missed == 0:
+            print("\nOh no ! Seems like you have lost !\n")
+        else:
+            print(f'\n"{self.active_phrase.phrase}"\n')
+            print("Yay !!! Congratulations you have won !!!\n")
